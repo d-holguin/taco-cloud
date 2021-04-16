6164ad37c0a09d8b5077f0b2ac0164b17dfe596c
KiwiCode-s <38774014+KiwiCode-s@users.noreply.github.com>
2021-04-15 20:39:23 -0700

    implemented design page

---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,15 @@
+/**
+ * @ author Dante
+ */
+package tacos;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class Taco {
+
+    private String name;
+
+    private List<Ingredient> ingredients;
+
+}
